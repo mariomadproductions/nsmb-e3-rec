a343c9fab0d99418e29f0036fd78a5f7ccb6e7a0
mariomadproductions <mariomadproductions@users.noreply.github.com>
2020-04-12 05:28:41 +0100

    made changes to mega_mario.cpp as suggested by TheGameratorT, removed extracted sound data and recreated sound data changes using Nitro Studio 2

--- source/mega_mario.cpp
+++ source/mega_mario.cpp
@@ -1,2 +1,2 @@
 void nsub_0209E038_ov_00() {} //Disable goomba/coin drop on ground-pound
-void nsub_0215F368_ov_36() {} //Disable bonus counter
+void repl_0215ED54_ov_36() {} //Do not spawn Mega Mushroom destruction counter
