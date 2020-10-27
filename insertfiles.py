4aa27d8c09cb391f385a1bf1fe7364add4c6176a
mariomadproductions <mariomadproductions@users.noreply.github.com>
2020-10-27 21:24:00 +0000

    changed insertfiles.py so it gets rom name from argument

--- insertfiles.py
+++ insertfiles.py
@@ -2,16 +2,19 @@ from sys import argv
 from pathlib import Path
 import ndspy.rom
 
-rom_filename = 'rom.nds'
+rom_filename = argv[1]
 rom = ndspy.rom.NintendoDSRom.fromFile(rom_filename)
 path_overrides_filename = 'source_nitrofs_path_overrides.txt'
 path_overrides = {}
 
 #Generate dictionary from filename overrides file
-with open(path_overrides_filename, 'r') as path_overrides_file:
-    for line in path_overrides_file:
-        line_interpreted = line.split('\n')[0].split(sep=',')
-        path_overrides[line_interpreted[0]] = int(line_interpreted[1])
+try:
+    with open(path_overrides_filename, 'r') as path_overrides_file:
+        for line in path_overrides_file:
+            line_interpreted = line.split('\n')[0].split(sep=',')
+            path_overrides[line_interpreted[0]] = int(line_interpreted[1])
+except FileNotFoundError:
+    pass
 
 #Insert files into ROM
 for path in Path('source_nitrofs').rglob('*.*'):
