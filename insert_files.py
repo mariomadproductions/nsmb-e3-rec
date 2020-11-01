6e13efa05624fc2003ca70624a08c38463f7134e
TheGameratorT <tiagolsilva14@gmail.com>
2020-11-01 12:07:01 +0000

    Revert accidental change.

--- insert_files.py
+++ insert_files.py
@@ -2,7 +2,7 @@ from sys import argv
 from pathlib import Path
 import ndspy.rom
 
-rom_filename = 'rom.nds'
+rom_filename = argv[1]
 rom = ndspy.rom.NintendoDSRom.fromFile(rom_filename)
 path_overrides_filename = 'source_nitrofs_path_overrides.txt'
 path_overrides = {}
@@ -12,6 +12,8 @@ with open(path_overrides_filename, 'r') as path_overrides_file:
     for line in path_overrides_file:
         line_interpreted = line.split('\n')[0].split(sep=',')
         path_overrides[line_interpreted[0]] = int(line_interpreted[1])
+except FileNotFoundError:
+    pass
 
 #Insert files into ROM
 for path in Path('source_nitrofs').rglob('*.*'):
