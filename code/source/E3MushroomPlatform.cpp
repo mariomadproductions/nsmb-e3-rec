30fddd156ddf6b9ff6b924f3a256beedf5bbd8e2
Tiago Silva <tiagolsilva14@gmail.com>
2023-02-01 18:01:31 +0000

    Fixed swinging rope glitched graphics
    Fixes #7

--- code/source/E3MushroomPlatform.cpp
+++ code/source/E3MushroomPlatform.cpp
@@ -28,8 +28,6 @@ ncp_call_ov 0x0217F608, 90
 ncp_repl(0x0217F504, 90, "ADD R1, R0, #0x20000")
 
 // Mushroom color
-ncp_repl(0x0217DD88, 90, "NOP")
-
 asm(R"(
 ncp_call_ov 0x0217DD94, 90
 	// Get actor to R0
@@ -55,21 +53,38 @@ ncp_call_ov 0x0217DD94, 90
 
 // Mushroom stem
 asm(R"(
-ncp_call_ov 0x02160BF4, 54
-	// Get actor to R1
-	LDR     R1, =0x560
-	SUB     R1, R0, R1
-	// Class Check
-	LDR     R3, [R1,#0xC]
-	CMP     R3, #195
-	MOVEQ   R3, #0
-	BXEQ    LR
-	// Texture Check
-	LDR     R3, [R1,#8]
-	ANDS    R3, R3, #0x10000000
-	MOVEQ   R3, #0
-	MOVNE   R3, #1
-	BX      LR
+ncp_jump_ov 0x0217E034, 90
+	// Get actor->settings to R0
+	LDR     R0, [R5,#(0x8 - 0x544)]
+	// Get "Fat Stalk" property
+	MOV     R0, R0, LSR#28
+	AND     R0, R0, #1
+	// Set property
+	LDR     R1, =StalkTexture
+	STR     R0, [R1]
+	// Return
+	MOV     R12, #0
+	B       0x0217E038
+
+ncp_jump_ov 0x0217E04C, 90
+	BL      0x0217DE58
+	// Reset property
+	MOV     R0, #0
+	LDR     R1, =StalkTexture
+	STR     R0, [R1]
+	B       0x0217E050
+
+ncp_jump_ov 0x02160BF4, 54
+	// Get property
+	LDR     R3, =StalkTexture
+	LDR     R3, [R3]
+	// Return
+	B       0x02160BF8
+
+.data
+.align 4
+StalkTexture:
+	.int    0
 )");
 
 ncp_repl(0x0217F5A0, 90, "NOP") // Do not setup corner 1
