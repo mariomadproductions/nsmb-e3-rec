51edd3847997d7766d4b98bc031124f53a8cfd47
Tiago Silva <tiagolsilva14@gmail.com>
2023-02-01 18:05:12 +0000

    Misc changes + more debug utils

--- code/source/Debug.cpp
+++ code/source/Debug.cpp
@@ -7,6 +7,9 @@
 namespace Debug
 {
 	bool collisionViewerEnabled = false;
+	fx32 playerDragAccelX = 0;
+	fx32 playerDragAccelY = 0;
+	bool draggingPlayer = false;
 
 	void update()
 	{
@@ -15,6 +18,58 @@ namespace Debug
 		if ((keysHeld & Keys::Select) && (keysPressed & Keys::X))
 		{
 			collisionViewerEnabled = !collisionViewerEnabled;
+		}
+
+		if (Scene::currentSceneID == u16(SceneID::Stage))
+		{
+			Player* player = Game::getPlayer(0);
+			if (keysHeld & Keys::X)
+			{
+				fx32 playerDragVelX = FX_Mul(0x1000, playerDragAccelX);
+				if (keysHeld & Keys::Horizontal)
+				{
+					if (playerDragAccelX < 0x1000)
+						playerDragAccelX = 0x1000;
+					if (keysHeld & Keys::Left)
+						player->position.x -= playerDragVelX;
+					else if (keysHeld & Keys::Right)
+						player->position.x += playerDragVelX;
+					if (playerDragVelX < 0x18000)
+						playerDragAccelX += 0x800;
+				}
+				else
+				{
+					playerDragAccelX = 0x1000;
+				}
+
+				fx32 playerDragVelY = FX_Mul(0x1000, playerDragAccelY);
+				if (keysHeld & Keys::Vertical)
+				{
+					if (playerDragAccelY < 0x1000)
+						playerDragAccelY = 0x1000;
+					if (keysHeld & Keys::Down)
+						player->position.y -= playerDragVelY;
+					else if (keysHeld & Keys::Up)
+						player->position.y += playerDragVelY;
+					if (playerDragVelY < 0x18000)
+						playerDragAccelY += 0x800;
+				}
+				else
+				{
+					playerDragAccelY = 0x1000;
+				}
+
+				if (!draggingPlayer)
+				{
+					player->beginCutscene(0);
+					draggingPlayer = true;
+				}
+			}
+			else if (draggingPlayer)
+			{
+				player->endCutscene();
+				draggingPlayer = false;
+			}
 		}
 	}
 
